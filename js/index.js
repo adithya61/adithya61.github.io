3490332c579554b26b1b283fd74fe493abd86728
adithya61 <zonic662@gmail.com>
2020-05-15 15:11:59 +0530

    my portfolio site

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/*
+
+<div class="pos-f-t">
+    <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark p-4">
+            <span class="text-muted">
+                <nav><a href="_blank"> HOME </a></nav>
+            </span>
+        </div>
+    </div>
+    <nav class="navbar navbar-dark bg-secondary ">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent"
+                aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </nav>
+</div>
+ */
